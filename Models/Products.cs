ff7b1125f60c5d8891782a0228bfd34a1031c3e9
ST10033895 <128804168+morishkaDhalip@users.noreply.github.com>
2024-03-26 12:12:35 +0200

    Add project files.

---
+++ Models/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace KhumaloCraft_Emporium.Models
+{
+    public class Products
+    {
+        public int Id { get;  set; }
+        public  string? Name { get;  set; }
+        public  string? Description { get;  set; }
+        public int Price { get;  set; }
+        public  string? ImageUrl { get;  set; }
+    }
+}
